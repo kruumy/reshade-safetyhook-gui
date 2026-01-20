c19196486e1220878dfd8ca58eb77a8ec617d049
admin <joshuampauls@gmail.com>
2026-01-20 13:22:42 -0500

    basic implementation

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,13 +1,36 @@
 #include <imgui.h>
 #include <reshade.hpp>
 #include <safetyhook.hpp>
+#include <string>
+#include <windows.h>
+
+static safetyhook::InlineHook g_hook;
+static char g_address_buffer[128] = "0x0";
+
+void hooked_function_callback()
+{
+    reshade::log::message(reshade::log::level::info, "Target address was called.");
+}
 
 static void draw_window(reshade::api::effect_runtime* runtime)
 {
-    ImGui::TextUnformatted("Some text");
+    ImGui::InputText("Target Address", g_address_buffer, sizeof(g_address_buffer));
+
+    if (ImGui::Button("Apply Hook"))
+    {
+        uintptr_t target_addr = std::stoull(g_address_buffer, nullptr, 16);
+        g_hook = safetyhook::create_inline(reinterpret_cast<void*>(target_addr), reinterpret_cast<void*>(hooked_function_callback));
+        reshade::log::message(reshade::log::level::info,"Successfully applied safetyhook");
+    }
+
+    if (ImGui::Button("Remove Hook"))
+    {
+        g_hook = {};
+        reshade::log::message(reshade::log::level::info, "Hook removed.");
+    }
 }
 
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID)
+extern "C" BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID)
 {
     switch (fdwReason)
     {
@@ -16,9 +39,10 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID)
         {
             return FALSE;
         }
-        reshade::register_overlay("safetyhook-gui", &draw_window);
+        reshade::register_overlay("SafetyHook GUI", &draw_window);
         break;
     case DLL_PROCESS_DETACH:
+        g_hook = {};
         reshade::unregister_addon(hinstDLL);
         break;
     }
