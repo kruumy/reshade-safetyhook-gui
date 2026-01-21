e8024e6ad9670b56df355d6fafafa5136a203a0d
kruumy <joshuampauls@gmail.com>
2026-01-21 16:57:03 -0500

    fix midhook trampoline -> destination

--- src/midhook_definition.h
+++ src/midhook_definition.h
@@ -3,12 +3,33 @@
 #include <safetyhook.hpp>
 #include <string>
 #include <format>
+#include <unordered_map>
 
 #if SAFETYHOOK_ARCH_X86_64
 #define IP_REG rip
 #elif SAFETYHOOK_ARCH_X86_32
 #define IP_REG eip
 #endif
+
+// Tag for m_hook (InlineHook)
+struct MidHookInlineTag
+{
+    typedef safetyhook::InlineHook safetyhook::MidHook::* type;
+};
+
+// Template to steal private members
+template <typename Tag, typename Tag::type M>
+struct PrivateMemberStealer
+{
+    friend typename Tag::type get_member(Tag)
+    {
+        return M;
+    }
+};
+
+template struct PrivateMemberStealer<MidHookInlineTag, &safetyhook::MidHook::m_hook>;
+safetyhook::InlineHook safetyhook::MidHook::* get_member(MidHookInlineTag);
+
 
 class midhook_definition
 {
@@ -19,54 +40,55 @@ public:
     void destination(SafetyHookContext& ctx)
     {
         std::string log_msg = std::format("Midhook triggered for object at target: 0x{:X}", reinterpret_cast<uintptr_t>(target));
-        std::string ip_msg = std::format("Actual IP at trigger: 0x{:X}", static_cast<uintptr_t>(ctx.IP_REG));
-
         reshade::log::message(reshade::log::level::info, log_msg.c_str());
-        reshade::log::message(reshade::log::level::info, ip_msg.c_str());
     }
 
     void enable()
     {
         if (target == nullptr) return;
 
-        registry[target] = this;
+        auto result = safetyhook::MidHook::create(target, &trampoline);
+        if (result)
+        {
+            hook = std::move(*result);
+            auto& m_hook = hook.*get_member(MidHookInlineTag{});
 
-        hook = safetyhook::create_mid(target, trampoline);
-        is_enabled = true;
+            instance_registry[m_hook.trampoline().address()] = this;
+
+            is_enabled = true;
+        }
     }
 
     void disable()
     {
         if (is_enabled)
         {
+            auto& m_hook = hook.*get_member(MidHookInlineTag{});
+            uintptr_t tramp_addr = m_hook.trampoline().address();
+
+            instance_registry.erase(tramp_addr);
             hook = {};
-            registry.erase(target);
             is_enabled = false;
         }
     }
 
+    
+
 private:
     SafetyHookMid hook;
 
-    inline static std::map<void*, midhook_definition*> registry;
+    inline static std::unordered_map<uintptr_t, midhook_definition*> instance_registry;
 
     static void trampoline(SafetyHookContext& ctx)
     {
-        void* current_ip = reinterpret_cast<void*>(ctx.IP_REG);
-        auto it = registry.find(current_ip);
-
-        if (it == registry.end() && !registry.empty())
+        if (instance_registry.contains(ctx.IP_REG))
         {
-            auto lb = registry.lower_bound(current_ip);
-            if (lb != registry.begin())
-            {
-                it = std::prev(lb);
-            }
+            midhook_definition* instance = instance_registry[ctx.IP_REG];
+            instance->destination(ctx);
+            return;
         }
 
-        if (it != registry.end())
-        {
-            it->second->destination(ctx);
-        }
+        std::string ip_msg = std::format("Could not find IP in registry: 0x{:X}", static_cast<uintptr_t>(ctx.IP_REG));
+        reshade::log::message(reshade::log::level::error, ip_msg.c_str());
     }
 };
